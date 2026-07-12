56ae803a04fa1f6d4c8bc4f17b91e9c137ebcf5b
jeevansanchez <166963895+jeevansanchez@users.noreply.github.com>
2026-07-12 17:48:49 -0400

    fixes

--- LSM6DSM/src/registers.h
+++ LSM6DSM/src/registers.h
@@ -14,34 +14,34 @@
 
 namespace LSM6DSM_REG {
     class Config {
-        constexpr uint8_t CTRL1_XL = 0x10; 
-        constexpr uint8_t CTRL2_G = 0x11; 
-        constexpr uint8_t CTRL3_C = 0x12; 
-        constexpr uint8_t CTRL4_C = 0x13; 
-        constexpr uint8_t CTRL5_C = 0x14; 
-        constexpr uint8_t CTRL6_C = 0x15; 
-        constexpr uint8_t CTRL7_G = 0x16; 
-        constexpr uint8_t CTRL8_XL = 0x17; 
-        constexpr uint8_t CTRL9_XL = 0x18; 
-        constexpr uint8_t CTRL10_C = 0x19; 
-        constexpr uint8_t WHO_AM_I = 0X0F; 
-        constexpr uint8_t STATUS_REG = 0X1E;
+       static constexpr uint8_t CTRL1_XL = 0x10; 
+       static constexpr uint8_t CTRL2_G = 0x11; 
+       static constexpr uint8_t CTRL3_C = 0x12; 
+       static constexpr uint8_t CTRL4_C = 0x13; 
+       static constexpr uint8_t CTRL5_C = 0x14; 
+       static constexpr uint8_t CTRL6_C = 0x15; 
+       static constexpr uint8_t CTRL7_G = 0x16; 
+       static constexpr uint8_t CTRL8_XL = 0x17; 
+       static constexpr uint8_t CTRL9_XL = 0x18; 
+       static constexpr uint8_t CTRL10_C = 0x19; 
+       static constexpr uint8_t WHO_AM_I = 0X0F; 
+       static constexpr uint8_t STATUS_REG = 0X1E;
 };
 
     class Data {
-        constexpr uint8_t OUT_TEMP_L = 0x20; 
-        constexpr uint8_t OUT_TEMP_H = 0x21; 
-        constexpr uint8_t OUTX_L_G = 0x22; 
-        constexpr uint8_t OUTX_H_G = 0x23; 
-        constexpr uint8_t OUTY_L_G = 0X24; 
-        constexpr uint8_t OUTY_H_G = 0X25; 
-        constexpr uint8_t OUTZ_L_G = 0x26; 
-        constexpr uint8_t OUTZ_H_G = 0x27; 
-        constexpr uint8_t OUTX_L_XL = 0x28; 
-        constexpr uint8_t OUTX_H_XL = 0x29; 
-        constexpr uint8_t OUTY_L_XL = 0X2A; 
-        constexpr uint8_t OUTY_H_XL = 0X2B; 
-        constexpr uint8_t OUTZ_L_XL = 0x2C; 
-        constexpr uint8_t OUTZ_H_G = 0x2D; 
+       static constexpr uint8_t OUT_TEMP_L = 0x20; 
+       static constexpr uint8_t OUT_TEMP_H = 0x21; 
+       static constexpr uint8_t OUTX_L_G = 0x22; 
+       static constexpr uint8_t OUTX_H_G = 0x23; 
+       static constexpr uint8_t OUTY_L_G = 0X24; 
+       static constexpr uint8_t OUTY_H_G = 0X25; 
+       static constexpr uint8_t OUTZ_L_G = 0x26; 
+       static constexpr uint8_t OUTZ_H_G = 0x27; 
+       static constexpr uint8_t OUTX_L_XL = 0x28; 
+       static constexpr uint8_t OUTX_H_XL = 0x29; 
+       static constexpr uint8_t OUTY_L_XL = 0X2A; 
+       static constexpr uint8_t OUTY_H_XL = 0X2B; 
+       static constexpr uint8_t OUTZ_L_XL = 0x2C; 
+       static constexpr uint8_t OUTZ_H_XL = 0x2D; 
     };
 };
